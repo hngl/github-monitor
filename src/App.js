6a31dee4649188f211d8a25034ae6e23d132d2fb
Jurriaan van Hengel <j.vanhengel@dation.nl>
2018-09-19 11:44:01 +0200

    Move Settings to full-screen modal

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import './App.css';
 import octokit from '@octokit/rest';
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
 import TextField from '@material-ui/core/TextField';
 import JobGrid from "./JobGrid";
 import MuiThemeProvider from "@material-ui/core/es/styles/MuiThemeProvider";
@@ -10,6 +9,12 @@ import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import AppBar from "@material-ui/core/AppBar/AppBar";
 import Toolbar from "@material-ui/core/Toolbar/Toolbar";
 import Typography from "@material-ui/core/Typography/Typography";
+import Dialog from "@material-ui/core/Dialog/Dialog";
+import IconButton from "@material-ui/core/IconButton/IconButton";
+import List from "@material-ui/core/List/List";
+import ListItem from "@material-ui/core/ListItem/ListItem";
+import Slide from "@material-ui/core/Slide/Slide";
+import CloseIcon from '@material-ui/icons/Close';
 
 const client = octokit();
 
@@ -17,13 +22,16 @@ const theme = createMuiTheme({palette: {type: 'dark'}});
 
 class App extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    let token = localStorage.getItem('token');
     this.state = {
       owner: localStorage.getItem('owner'),
       repo: localStorage.getItem('repo'),
       branches: [],
-      token: localStorage.getItem('token')
+      token: token,
+      isSettingsOpen: !token
     };
     if(this.state.token) {
       this.fetchBranches()
@@ -51,49 +59,73 @@ class App extends Component {
     this.setState({
       owner: owner,
       repo: repo,
-      token: token
+      token: token,
+      isSettingsOpen: false
     });
     this.fetchBranches()
   };
 
+  handleOpenSettings = () => {
+    this.setState({isSettingsOpen: true})
+  };
+
+  handleCloseSettings = () => {
+    this.setState({isSettingsOpen: false})
+  };
+
   render() {
+    let classes = {};
     return (
         <MuiThemeProvider theme={theme}>
           <AppBar position="static" color="default">
             <Toolbar>
-              <Typography variant="title" color="inherit">
+              <Typography variant="title" color="inherit" style={{flexGrow: 1}}>
                 {this.state.owner} / {this.state.repo}
               </Typography>
+              <Button onClick={this.handleOpenSettings}>Settings</Button>
             </Toolbar>
           </AppBar>
-            <JobGrid client={client} {...this.state}/>
-            <Row>
-              <Col>
-                <Card className="settings">
-                    <form onSubmit={this.handleSettingsSubmit}>
-                      <TextField label="Owner" name="owner" defaultValue={this.state.owner}/>
-                      <TextField label="Repo" name="repo" defaultValue={this.state.repo}/>
-                      <TextField label="Token" name="token" defaultValue={this.state.token}/>
-                      <Button>Save</Button>
-                    </form>
-                </Card>
-              </Col>
-            </Row>
+          <JobGrid client={client} {...this.state}/>
+          <Dialog
+              fullScreen
+              open={this.state.isSettingsOpen}
+              onClose={this.handleCloseSettings}
+              TransitionComponent={SlideTransition}
+          >
+            <form onSubmit={this.handleSettingsSubmit}>
+              <AppBar color="default" style={{position: 'relative'}}>
+                <Toolbar>
+                  <IconButton color="inherit" onClick={this.handleCloseSettings} aria-label="Close">
+                    <CloseIcon/>
+                  </IconButton>
+                  <Typography variant="title" color="inherit" style={{flexGrow: 1}}>
+                    Settings
+                  </Typography>
+                  <Button color="inherit" type="submit">
+                    Save
+                  </Button>
+                </Toolbar>
+              </AppBar>
+              <List>
+                <ListItem>
+                  <TextField label="Owner" name="owner" defaultValue={this.state.owner}/>
+                </ListItem>
+                <ListItem>
+                  <TextField label="Repo" name="repo" defaultValue={this.state.repo}/>
+                </ListItem>
+                <ListItem>
+                  <TextField label="Token" name="token" defaultValue={this.state.token}/>
+                </ListItem>
+              </List>
+            </form>
+          </Dialog>
         </MuiThemeProvider>
     );
   }
 }
 
-function Col(props) {
-  return(<div>{props.children}</div>);
-}
-
-function Row(props) {
-  return(<div>{props.children}</div>);
-}
-
-function Container(props) {
-  return(<div>{props.children}</div>);
+function SlideTransition(props) {
+  return <Slide direction="up" {...props} />;
 }
 
 export default App;
