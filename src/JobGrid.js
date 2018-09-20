73d375ced975669d957bc45e61fd3410c3972681
Jurriaan van Hengel <j.vanhengel@dation.nl>
2018-09-20 23:18:12 +0200

    Sort Cards by recency

--- src/JobGrid.js
+++ src/JobGrid.js
@@ -3,6 +3,7 @@ import JobCard from "./JobCard";
 import React, {Component} from "react";
 import './JobGrid.css';
 import shortSha from './shortSha'
+import moment from "moment";
 
 export default class JobGrid extends Component {
 
@@ -31,7 +32,7 @@ export default class JobGrid extends Component {
           this.fetchCommit(branch.commit.sha);
         }
       })
-    }).catch(resp => {console.error('Failed to fetch branches', resp)})
+    }).catch(console.error)
   };
 
   fetchCommit(sha) {
@@ -45,7 +46,7 @@ export default class JobGrid extends Component {
           console.debug(`Fetched commit ${shortSha(sha)}`, data);
           this.setState({commits: new Map(this.state.commits).set(sha, data)});
           this.fetchStatus(sha)
-        }).catch(err => console.error(err))
+        }).catch(console.error)
   }
 
   fetchStatus(sha) {
@@ -56,18 +57,40 @@ export default class JobGrid extends Component {
     })
         .then(({data}) => {
           console.debug(`Fetched status for ${shortSha(sha)}`, data);
-          let newCommit = Object.assign({}, this.state.commits.get(sha), {state: data.state, statuses: data.statuses});
+          let newCommit = Object.assign({}, this.getCommit(sha), {state: data.state, statuses: data.statuses});
           this.setState({commits: new Map(this.state.commits).set(sha, newCommit)})
-        }).catch(err => console.error(err))
+        }).catch(console.error)
+  }
+
+  getCommit(sha) {
+    return this.state.commits.get(sha);
   }
 
   render() {
+    let sortedBranches = this.state.branches.slice().sort((branchA, branchB) => {
+      if(!this.state.commits.has(branchA.commit.sha) && !this.state.commits.has(branchB.commit.sha)) {
+        return 0; // Do nothing
+      }
+      if(!this.state.commits.has(branchA.commit.sha)) {
+        return 2; // Sort B to a lower index
+      }
+      if(!this.state.commits.has(branchB.commit.sha)) {
+        return -2; // Sort B to a lower index
+      }
+      let dateA = this.getCommit(branchA.commit.sha).commit.author.date;
+      let dateB = this.getCommit(branchB.commit.sha).commit.author.date;
+      if(dateA === dateB) {
+        return 0; // Do nothing
+      }
+      return moment(dateA).isBefore(dateB) ? 1 : -1;
+    });
+
     return (
         <Grid container spacing={16} className="job-grid">
-          {this.state.branches.map(branch => {
+          {sortedBranches.map(branch => {
             return (
                 <Grid item xs={4} key={branch.name}>
-                  <JobCard branch={branch} commit={this.state.commits.get(branch.commit.sha)}/>
+                  <JobCard branch={branch} commit={this.getCommit(branch.commit.sha)}/>
                 </Grid>
             )
           })}
